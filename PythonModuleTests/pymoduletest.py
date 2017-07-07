a5da3e02365125532a2c84310110ed0b7cb223a6
jmacey <jmacey@bournemouth.ac.uk>
2017-07-07 11:10:20 +0100

    added cmake for bullet

--- PythonModuleTests/pymoduletest.py
+++ PythonModuleTests/pymoduletest.py
@@ -17,6 +17,17 @@ class MyTest(unittest.TestCase):
     def testNumpy(self) :
 	self.assertTrue(testImport('numpy'))
 	self.assertTrue(testImport('scipy'))
+    def testOIIO(self) :
+	self.assertTrue(testImport('OpenImageIO'))
 
+    def testOpenEXR(self) :
+	self.assertTrue(testImport('OpenEXR'))
+	self.assertTrue(testImport('Imath'))
+
+    def testAlembic(self) :
+	self.assertTrue(testImport('alembic'))
+    
+    def testPyQt(self) :
+	self.assertTrue(testImport('PyQt5'))
 if __name__ == '__main__':
     unittest.main()
